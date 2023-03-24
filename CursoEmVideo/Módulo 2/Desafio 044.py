e802bafedc13b887a5e273aa5224a15b7ceb5bb0
andre matos <andrermatos90@gmail.com>
2023-03-23 23:18:40 -0300

    DESAFIO 044

--- CursoEmVideo/Módulo 2/Desafio 044.py
+++ CursoEmVideo/Módulo 2/Desafio 044.py
@@ -5,3 +5,22 @@ considerando o seu preço normal  e condição de pagamento:
 - à vista no cartão: 5% de desconto
 - em até 2x no cartão: preço normal
 - 3x ou mais no cartão: 20% de juros'''
+
+preco = float(input('Valor do produto:'))
+forma = int(input('Digite a forma de pagamento: \n 1 - A vista Dinheiro/Cheque\n 2 - A vista no Cartão\n 3 - 2X no Cartão\n 4 - 3x No cartão:\n'))
+
+
+
+if forma == 1:
+    print('Total a pagar: R${:.2f}'.format(abs((preco / 100 * 10) - preco)))
+
+elif forma == 2:
+    print('Total a pagar: R${:.2f}'.format(abs((preco / 100 *5) - preco)))
+
+elif forma == 3:
+    print('Total a pagar: R${:.2f}'.format(preco))
+
+else:
+    print('Total a pagar: R${:.2f}'.format(abs((preco / 100 + 20)  + preco)))
+
+
--- CursoEmVideo/Módulo 2/Desafio 045.py
+++ CursoEmVideo/Módulo 2/Desafio 045.py
@@ -1,1 +1,48 @@
 '''Crie um prorgama que jogue jokenpo com você'''
+
+from random import randint
+from time import sleep
+
+numero = randint(0,2)
+
+if numero == 0:
+    computador = 'pedra'
+
+elif numero == 1:
+    computador = 'tesoura' 
+
+else:
+    computador = 'papel'
+
+
+
+usuario = str(input('Digite "pedra, "tesoura" ou "papel": ')).lower()
+sleep(1)
+print('JO')
+sleep(1)
+print('KEN')
+sleep(1)
+print('PO')
+sleep(1)
+
+print(computador)
+sleep(1)
+
+
+if computador == usuario:
+    print('Ninguem ganha!')
+elif numero == 0 and usuario == 'tesoura':
+    print('Eu venci você!')
+elif numero == 0 and usuario =='papel':
+    print('Você me venceu!')
+elif numero == 1 and usuario == 'pedra':
+     print('Você me venceu!')
+elif numero == 1 and usuario =='papel':
+       print('Eu venci você!')
+elif numero == 2 and usuario == 'pedra':
+       print('Eu venci você!')
+elif numero == 2 and usuario == 'tesoura':
+     print('Você me venceu!')
+
+
+
