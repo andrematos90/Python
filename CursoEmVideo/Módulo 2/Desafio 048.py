06653934d9780994145acc834701f331f4f095ab
André Matos <andrermatos90@gmail.com>
2023-03-30 08:57:38 -0300

    Desafio

--- CursoEmVideo/Módulo 2/Desafio 048.py
+++ CursoEmVideo/Módulo 2/Desafio 048.py
@@ -1,7 +1,12 @@
 '''Faça um programa que calcule a soma entre todos os números impares que são múltiplos de três
 e que se encontram no intervalo de 1 até 500'''
 
-for n in range (0, 501, 3):
-    for n in range (0, 501, n+n):
-        if n % 2 == 0:
-            print(n)
+soma = 0
+cont =  0
+
+for n in range (1, 501, 2):
+    if n % 3 == 0:
+        soma = soma + n
+        cont = cont + 1
+print('A soma entre todos os {} valores encontrados é: {}'.format(cont, soma))
+
--- CursoEmVideo/Módulo 2/Desafio 049.py
+++ CursoEmVideo/Módulo 2/Desafio 049.py
@@ -1,2 +1,8 @@
-'''Refaça o DESAFIO 009, mostrando a tabuada de um número que p usuário escolher,
+'''Refaça o DESAFIO 009, mostrando a tabuada de um número que o usuário escolher,
 só que agora utilizando laço for.'''
+
+n = int(input('Número: '))
+
+for n in range (0, 10, *n):
+
+ print(n)
