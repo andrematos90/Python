f4a84eb0feb45bd1c637c2c45a3d47f7823d490a
André Matos <andrermatos90@gmail.com>
2023-04-03 16:15:32 -0300

    Desafio

--- CursoEmVideo/Módulo 2/Desafio 052.py
+++ CursoEmVideo/Módulo 2/Desafio 052.py
@@ -1,3 +1,5 @@
 '''Faça um programa que leia um número inteiro e diga se ele é primo ou não'''
 
 num = int(input('Digite um numero: '))
+
+for c in range (1, num + 1):
--- CursoEmVideo/Módulo 2/Testes.py
+++ CursoEmVideo/Módulo 2/Testes.py
@@ -1,7 +1,4 @@
-import datetime
 
-
-data_atual = datetime.datetime.now()
-print(data_atual)
-
-print ( "Data e hora no formato inteiro:" ,  int ( data_atual. strftime ( "%Y" ) ) )
+for c in range(0,6):
+    print('oi')
+    print('fim')
