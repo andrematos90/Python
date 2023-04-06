14e9ea55557399604a988d7383f2a48dd71d091b
André Matos <andrermatos90@gmail.com>
2023-04-06 16:46:09 -0300

    Desafio 059

--- CursoEmVideo/Módulo 2/Desafio 059.py
+++ CursoEmVideo/Módulo 2/Desafio 059.py
@@ -8,4 +8,51 @@ Crie um programa que leia dois valores e mostre um menu na tela:
 [5] Sair do programa
 
 Seu programa deverá realizar a operação solicitada em cada caso.
+
 '''
+from time import sleep
+
+num1 = int(input('Digite um numero: '))
+num2 = int(input('Digite o segundo número: '))
+opcao = 0
+
+while opcao != 5 :
+    print(''' Digite o número da opcão desejada
+     [1] Somar
+     [2] Multiplicar
+     [3] Maior
+     [4] Novos números
+     [5] Sair do programa
+     
+     Qual a opção Escolhida: ''')
+
+
+    opcao = int(input('Digite o número da opção: '))
+
+    if opcao == 1:
+        sleep(2)
+        print('A soma é {}'.format(num1 + num2))
+    elif opcao == 2:
+        print('A multiplicação é {}'.format(num1 * num1))
+    elif opcao == 3:
+        if num1 > num2:
+            print('O maior número é {}'.format(num1))
+        elif num2 > num1:
+            print('O maior número é o {}'.format(num2))
+        else:
+            print('Os numeros são iguais!')
+    elif opcao == 4:
+        num1 = int(input('Digite o primeiro numero novamente: '))
+        num2 = int(input('Digite o segundo número novamente: '))
+    elif opcao == 5:
+        print('FIM')
+        break
+    else:
+        print('Opção inexistente!')
+        sleep(2)
+
+ 
+
+
+
+
--- CursoEmVideo/Módulo 2/Testes.py
+++ CursoEmVideo/Módulo 2/Testes.py
@@ -1,17 +1,42 @@
-'''Faça um programa que leia um número inteiro e diga se ele é primo ou não'''
+'''
+Crie um programa que leia dois valores e mostre um menu na tela:
 
-n = int(input('Digite um número: '))
-tot = 0
+[1] Somar
+[2] Multiplicar
+[3] Maior
+[4] Novos números
+[5] Sair do programa
 
-for c in range (1, n+1):
-    if n % c == 0:
-       print('\033[33m', end=' ')
-       tot += 1 # é o mesmo que tot = tot + 1
-    else:
-        print('\033[34m', end=' ')
-    print('{}'.format(c), end=' ')
-print('\n\033[m O número {} foi divísivel por {} vezes'.format(n, tot))
+Seu programa deverá realizar a operação solicitada em cada caso.
+'''
+opcao = 0
 
+while opcao >= 0 and opcao < 4:
+    num1 = int(input('Digite o primeiro número: '))
+    num2 = int(input('Digite o primeiro número: '))
+    opcao = int(input('''
+     Digite o número da opcão desejada
+     [1] Somar
+     [2] Multiplicar
+     [3] Maior
+     [4] Novos números
+     [5] Sair do programa
+     
+     Qual a opção Escolhida: 
+     '''))
+    
+    if opcao == 1:
+        print('A soma é {}'.format(num1 + num2))
+    elif opcao == 2:
+        print('A multiplicação é {}'.format(num1 * num1))
+    elif opcao == 3:
+        if num1 > num2:
+            print('O maior número é {}'.format(num1))
+        else:
+            print('O maior número é o {}'.format(num2))
+    elif opcao == 4:
+        del num1, num2
+        opcao == 0
 
 
 
