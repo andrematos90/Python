d59aac74c4871223d35e2e30ee7f209698257307
André Matos <andrermatos90@gmail.com>
2023-04-06 09:34:10 -0300

    Desafio 057

---
+++ CursoEmVideo/Módulo 2/Desafio 057.py
@@ -0,0 +1,9 @@
+'''Faça um prorama que leia o sexo de uma pessoa, mas só aceite os valores 'M' ou 'F' .
+Caso contrário, peça a digitação novamente até ter um valor correto.'''
+
+sexo = str(input('Digite o sexo M/F: ').strip().upper()[0])  #strip tira os espaços, upper deixa maicuscolo, [0] pega só a primeira letra 
+while sexo not in 'MF':
+    sexo = input('O sexo deve ser M ou F: ')
+print('Registrado com sucesso')
+
+
