96ced08fa276e3c201e651136aaa858507694102
André Matos <andrermatos90@gmail.com>
2023-04-11 09:14:11 -0300

    Desafio 065

--- CursoEmVideo/Módulo 2/Desafio 065.py
+++ CursoEmVideo/Módulo 2/Desafio 065.py
@@ -4,19 +4,36 @@ e qual foi o maior e o menor valor lido. O programa deve perguntar ao usúario s
 a digitar valores
 '''
 
-num = int(input("Digite o primeiro termo da PA: "))
-cont = 1
-mais = 10
-total = 1
-resposta = 'S'
 
-while resposta != 'N':  
-    total = total + mais
-    while cont <= total:
-        print('{} '.format((termo)))
-        termo += razao
-        cont += 1
-    print('Pausa')
-    resposta = int(input('Quer continuar? '))
-print('fim')
+r = 's'
+c = 0
+m = 0
+s = 0
+menor = 0
+maior = 0
+
+while r in 'Ss':
+    n = int(input('Digite um número: '))
+    c+=1
+    s = s + n
+    if c == 1:
+        maior = n
+        menor = n
+    else:
+        if n > maior:
+            maior = n
+        if n < menor:
+            menor = n
+    r = str(input('Quer continuar digitando s/n? ')).upper().strip()[0]
+m = s / c
+print('Você digitou {} números'.format(c))
+print('A Média entre os números somados é {:.2f}'.format(m))
+print('O maior valor digitado foi {}'.format(maior))
+print('O menor valor digitado foi {}'.format(menor))
+
+
+
+# strip()[0] remove os espaços e considera só a primeira letra
+
+
 
