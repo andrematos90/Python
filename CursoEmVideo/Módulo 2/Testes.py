9e58245821cd9a564c022c4dd9f6bd9510609923
André Matos <andrermatos90@gmail.com>
2023-04-17 10:09:05 -0300

    Desafio 076

--- CursoEmVideo/Módulo 2/Testes.py
+++ CursoEmVideo/Módulo 2/Testes.py
@@ -1,12 +1,25 @@
-palavra = ('abacaxi', 'banana', 'laranja', 'limao', 'morango')
+Produtos = ('ovo', 18.99, 'feijao', 5.88, 'carne', 10.99)
 
-vogais = []
-# Iteração sobre cada letra da palavra
-for letra in palavra:
-    # Verifica se a letra é uma vogal
-    if letra.lower() in 'aeiou':
-        # Adiciona a vogal à lista de vogais da palavra
-        vogais.append(letra)
+'''
+o loop for é iniciado, utilizando a função range() 
+para iterar sobre cada elemento da lista "Produtos"
+ou seja vai do "0" até o tamanho da lista "5".
 
-# Imprime a palavra e suas vogais correspondentes
-print(f'A palavra "{palavra}" tem as seguintes vogais: {vogais}')
+A condição "if" verifica se o valor da variável
+"pos" (que é o índice atual do elemento da lista)
+é divisível por 2. Se for, significa que a posição 
+é par e o código dentro do bloco if é executado.'''
+
+for pos in range(0, len(Produtos)):
+    if pos % 2 == 0:
+       print(f'{Produtos[pos]:.<30}', end='')  #formata com 30 caracteres
+                                               #alinha o texto a esquerda e enche de pontos
+    else:
+        print(f'R${Produtos[pos]:>7.2f}')
+'''
+O bloco if simplesmente imprime o elemento da
+ lista que está na posição "pos". Como a condição
+   if garante que só serão impressos os elementos 
+   de posição par, o código imprimirá somente os 
+   nomes dos produtos (e não os preços).
+'''
--- CursoEmVideo/Módulo 3/Desafio 076.py
+++ CursoEmVideo/Módulo 3/Desafio 076.py
@@ -33,5 +33,38 @@ tupla (ou seja, o nome do produto), formata a string de saída para que o nome s
 alinhado à esquerda com pontos para preencher o espaço restante até 30 caracteres, 
 seguido pela string "R$" e o preço do produto alinhado à direita com zeros para 
 preencher o espaço restante até 7 caracteres e duas casas decimais.
-print('=' * 40) - imprime uma linha horizontal de 40 caracteres de comprimento.'''
+print('=' * 40) - imprime uma linha horizontal de 40 caracteres de comprimento.
+
+
+
+
+                                COÓDIGO GUANABARA
+
+
+Produtos = ('ovo', 18.99, 'feijao', 5.88, 'carne', 10.99)
+
+
+o loop for é iniciado, utilizando a função range() 
+para iterar sobre cada elemento da lista "Produtos"
+ou seja vai do "0" até o tamanho da lista "5".
+
+A condição "if" verifica se o valor da variável
+"pos" (que é o índice atual do elemento da lista)
+é divisível por 2. Se for, significa que a posição 
+é par e o código dentro do bloco if é executado.
+
+for pos in range(0, len(Produtos)):
+    if pos % 2 == 0:
+       print(f'{Produtos[pos]:.<30}', end='')  #formata com 30 caracteres
+                                               #alinha o texto a esquerda e enche de pontos
+    else:
+        print(f'R${Produtos[pos]:>7.2f}')
+
+O bloco if simplesmente imprime o elemento da
+ lista que está na posição "pos". Como a condição
+   if garante que só serão impressos os elementos 
+   de posição par, o código imprimirá somente os 
+   nomes dos produtos (e não os preços).
+'''
+
 
