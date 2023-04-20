98db035e54cebe9aa8f0d60ff8037c5ebfaea628
André Matos <andrermatos90@gmail.com>
2023-04-20 15:37:48 -0300

    Aula 17

--- CursoEmVideo/Módulo 3/Testes.py
+++ CursoEmVideo/Módulo 3/Testes.py
@@ -1,20 +1,25 @@
-numeros_digitados = []
-impares = []
-pares = []
+'''
+Crie um programa onde o usuário digite uma expressão qualquer que use parênteses.
+Seu programa deverá analisar se a expressão passada está com os parêntes abertos e fechados
+na ordem correta.'''
 
-while True:
-    numeros_digitados.append(int(input('Digite um número: ')))
-    resposta = str(input('Quer continuar? [S/N] ?'))
-    if resposta in 'nN':
-     break
+expressao = str(input('Digite uma expressão: '))
+pilha = []
 
-for indice, valor in enumerate(numeros_digitados):
-    if valor % 2 == 0:
-        pares.append(valor)
-    else:
-        impares.append(valor)
 
-print(f'Números digitados! {numeros_digitados}')
-print(f'pares: {pares}')
-print(f'impares: {impares}')
+for c in expressao:
+    if c == '(':
+        pilha.append(c)
+        if c == ')':
+            if len(pilha) == 0:
+                print('Expressão inválida : há um parêntese de fechamento sem um parêntese de abertura correspondente.')
+                break
+            else:
+                pilha.pop()
+if len(pilha) == 0:
+     print('Expressão válida!')
+else:
+     print('Expressão inválida!')
+            
+
 
