6c3051b4a764feb9521d6856a18c77dd5c665106
André Matos <andrermatos90@gmail.com>
2023-04-25 09:17:23 -0300

    Aula 17

--- CursoEmVideo/Módulo 3/Testes.py
+++ CursoEmVideo/Módulo 3/Testes.py
@@ -1,32 +1,18 @@
 '''
-78 - Crie um programa que leia 5 valores numéricos e guarde em uma lista
-No final, mostre qual foi o maior e o menor valor digitado e as respectivas
-posições na lista'''
+79 - Crie um programa que leia vários números e os cadastre-os em 
+uma lista. Caso o número ja exista ele não sera adicionado.
+No final, serão exibidos todos os valores únicos digitados,
+em ordem de crescente'''
 
-numeros  = []
-maior = menor = 0
 
-for c in range(0, 5):
-    numeros.append(int(input(f'Digite o numero para a posição {c}: ')))
-    if c == 0: 
-        maior = menor = numeros[c]
-    else:
-        if numeros[c] > maior:
-            maior = numeros[c]
-        if numeros[c] < menor:
-            menor = numeros[c]
+lista = []
+copia = lista[:]
 
-print(f'Lista: {numeros}')
+while True:
+    lista.append(int(input('Número: ')))
+    resposta = str(input('Quer continuar? [S/N]'))
+    if resposta in 'nN':
+        break
 
-for i, v in enumerate(numeros):
-    if v == maior:
-        print()
-     
-print(f'na posição {i} temos {maior} como maior')
-
-for i, v in enumerate(numeros):
-    if v == menor:
-        print()
-
-print(f'na posição {i} temos { menor} como menor')
-
+   
+print(copia)
