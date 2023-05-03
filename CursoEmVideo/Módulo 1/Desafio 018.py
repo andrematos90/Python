3786fb069e04c091ffd48ae82c486c73201feca1
André Matos <103394184+andrematos90@users.noreply.github.com>
2023-05-02 23:13:47 -0300

    Desafio 018

--- CursoEmVideo/Módulo 1/Desafio 018.py
+++ CursoEmVideo/Módulo 1/Desafio 018.py
@@ -12,3 +12,13 @@ tangente = math.tan(A)
 print('Seno: {:.2f}' .format(seno))
 print('Cosseno: {:.2f}'.format(cosseno))
 print('Tangente: {:.2f}'.format(tangente))
+
+'''
+outra forma 
+
+import math
+
+angulo = int(input('Digite o angulo: '))
+print(f'Seno: {math.sin(angulo):.2f}')
+print(f'Cosseno: {math.cos(angulo):.2f}')
+print(f'Tangente: {math.tan(angulo):.2f}')'''
--- CursoEmVideo/Módulo 1/testes.py
+++ CursoEmVideo/Módulo 1/testes.py
@@ -1,7 +1,8 @@
-#Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triangulo retangulo
-#calcule e mostre o tamanho da hipotenusa.
-from math import sqrt
-oposto = float(input('Cateto oposto: '))
-adjacente  = float(input('Cateto adjacente: '))
-hipotenusa = sqrt(oposto **2 + adjacente **2) 
-print(f'A hipotenusa é {(hipotenusa)}')
+# digite um angulo e calcule o seno,cosseno e tangente desse angulo
+
+import math
+
+angulo = int(input('Digite o angulo: '))
+print(f'Seno: {math.sin(angulo):.2f}')
+print(f'Cosseno: {math.cos(angulo):.2f}')
+print(f'Tangente: {math.tan(angulo):.2f}')
