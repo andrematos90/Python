997dd57bb7eceaa61f0ada499676b9b2f8a88058
André Matos <103394184+andrematos90@users.noreply.github.com>
2023-05-06 09:38:29 -0300

    Exercicios

--- CursoEmVideo/Exercicios.py
+++ CursoEmVideo/Exercicios.py
@@ -182,7 +182,14 @@ e indique sua categoria de acorda com a sua idade:
 - isósceles: dois lados iguais
 - escaleno: todos os lados diferentes'''
 
-'''43 - Crie um programa que leia uma frase e diga se ela ela é um palíndromo, desconsiderando os espaços'''
+'''43 -Desenvolva uma lógica que leia o peso e a altura de uma pessoa, calcule seu IMC E mostre
+seu status, se acordo com a tabela abaixo:
+
+- Abaixo de 18.5: Abaixo do peso
+- Entre 18.5 e 25: Peso ideal 
+- 25 até 30: Sobrepeso
+- 30 até 40: Obesidade
+- Acima de 40: Obesidade Mórbida'''
 
 '''44 - Elabore um programa que calcule o valor a ser pago por um prouto, 
 considerando o seu preço normal  e condição de pagamento:
