d3ff9c9cb924739cc7a5939a2c5590ad7b9475a1
André Matos <103394184+andrematos90@users.noreply.github.com>
2023-05-08 21:01:14 -0300

    Desafio 050

--- CursoEmVideo/Módulo 2/Desafio 060.py
+++ CursoEmVideo/Módulo 2/Desafio 060.py
@@ -10,6 +10,24 @@ f = factorial(n)
 
 print('O Fatorial de {} é {}'.format(n, f))
 
+'''
+outra forma
+
+from time import sleep
+
+n = int(input('Digite um número para visualizar seu fatorial: '))
+c = n
+f = 1
+
+print('Caluculando fatorial de {}!'.format(n))
+sleep(2)
+
+while c > 0:
+    print('{} '.format(c), end='')
+    print(' x ' if c > 1 else '= ', end='')
+    f = f * c
+    c = c - 1
+print(f)'''
 
 
 
--- CursoEmVideo/testes.py
+++ CursoEmVideo/testes.py
@@ -1,78 +1,9 @@
-'''
-Crie um programa que leia dois valores e mostre um menu na tela:
+'''Faça um programa que leia um número inteiro e mostre seu fatorial
 
-[1] Somar
-[2] Multiplicar
-[3] Maior
-[4] Novos números
-[5] Sair do programa
+Ex: 5*4*3*2*1 = 120'''
 
-Seu programa deverá realizar a operação solicitada em cada caso.
+import math
 
-'''
-from time import sleep
-import os
-while True:
-    n1 = int(input('Primeiro valor: '))
-    n2 = int(input('Segundo valor: '))
-    print()
-    print('Qual operaçãp deseja realizar: ')
-    operacao = int(input('''
-    [1] Somar
-    [2] Multiplicar
-    [3] Maior
-    [4] Novos números
-    [5] Sair do programa 
-    ''' ))
-    if operacao == 1:
-        print(f'Soma de {n1} + {n2} = {n1 + n2}')
-    elif operacao == 2:
-        print(f'Multiplicação de {n1} x {n2} = {n1 * n2}')
-    elif operacao == 3:
-        maior = 0
-        if n1 > n2:
-            maior = n1
-        else:
-            maior = n2
-        print(f'O maior valor entre {n1} e {n2} é {maior}')
-    elif operacao == 4:
-        continue
-    elif operacao == 5:
-        print('Saindo do programa...')
-        print('Encerrando .')
-        sleep(1)
-        os.system('cls')
-        print('Encerrando ..')
-        sleep(1)
-        os.system('cls')
-        print('Encerrando ...')
-        sleep(1)
-        os.system('cls')
-        print('Encerrando ..')
-        sleep(1)
-        os.system('cls')
-        print('Encerrando .')
-        sleep
-        os.system('cls')
-        print('Encerrando ..')
-        sleep(1)
-        os.system('cls')
-        print('Encerrando ...')
-        sleep(1)
-        os.system('cls')
-        print('Encerrando ..')
-        sleep(1)
-        os.system('cls')
-        print('Encerrando .')
-        sleep(1)
-        os.system('cls')
-        print('FIM')
-        sleep(1)
-        break
-     
+numero = int(input('Número: '))
 
-
-
-
-
-    
+print(f'O fatorial de {numero} é {math.factorial(numero)}')
