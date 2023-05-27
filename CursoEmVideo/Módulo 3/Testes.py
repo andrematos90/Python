6fc41ce2d0355565e83f3ea1e1bd6decfdb79dec
André Matos <103394184+andrematos90@users.noreply.github.com>
2023-05-26 23:46:18 -0300

    Aula 19

--- CursoEmVideo/Módulo 3/Testes.py
+++ CursoEmVideo/Módulo 3/Testes.py
@@ -1,5 +1,17 @@
-lanche = ('pizza', 'churrasco', 'açai', 'camarão')
+'''
+86 - Crie um programa que crie uma matriz de dimensão 3x3 e preencha com valores
+lidos pelo teclado.
+No final, motre a matriz na tela com a formatação correta.
+'''
+
+matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+for linha in range(3):
+    for coluna in range(3):
+        matriz[linha][coluna] = int(input(f'numero para a posição: {linha}:{coluna}: '))
+print('=-' * 30)
+for linha in range(3):
+    for coluna in range(3):
+        print(f'[{matriz[linha][coluna]}]', end='')
+    print()
 
 
-for pos, comida in enumerate(lanche):
-        print(f'Comida {comida} na posição {pos}')
