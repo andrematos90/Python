cda955cbc09c336c63a6c78e5344e65cc1174003
André Matos <103394184+andrematos90@users.noreply.github.com>
2023-05-29 09:12:41 -0300

    Desafio 090

---
+++ CursoEmVideo/Módulo 3/Desafio 090.py
@@ -0,0 +1,11 @@
+''' 90 - Faça um programa que leia o nome de média de um aluno, guardando tambem a situação em um dicionário. No final, mostre o conteúdo da estrutura na tela.'''
+
+Aluno = {}
+
+Aluno['nome'] = str(input('Nome do aluno: '))
+Aluno['media'] = int(input(f'Média de {Aluno["nome"]}: '))
+
+if Aluno['media'] > 7:
+    print(f'{Aluno["nome"]} está aprovado!')
+else:
+    print(f'{Aluno["nome"]} está reprovado!')
--- CursoEmVideo/Módulo 3/Testes.py
+++ CursoEmVideo/Módulo 3/Testes.py
@@ -1,17 +1,6 @@
-'''
-86 - Crie um programa que crie uma matriz de dimensão 3x3 e preencha com valores
-lidos pelo teclado.
-No final, motre a matriz na tela com a formatação correta.
-'''
+filme = {'titulo': 'star wats',
+         'ano': 1977,
+         'diretor': 'george lucas'}
 
-matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-for linha in range(3):
-    for coluna in range(3):
-        matriz[linha][coluna] = int(input(f'numero para a posição: {linha}:{coluna}: '))
-print('=-' * 30)
-for linha in range(3):
-    for coluna in range(3):
-        print(f'[{matriz[linha][coluna]}]', end='')
-    print()
-
-
+for chave, valor in filme.items():
+    print(f'{chave} = {valor}')
