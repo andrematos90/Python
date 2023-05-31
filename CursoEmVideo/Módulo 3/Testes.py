db619aca03eb9ef8f6f49f64a1be9b14f3c5fe84
André Matos <103394184+andrematos90@users.noreply.github.com>
2023-05-30 23:25:26 -0300

    Testes

--- CursoEmVideo/Módulo 3/Testes.py
+++ CursoEmVideo/Módulo 3/Testes.py
@@ -1,16 +1,36 @@
-'''93 - Crie um programa que gerencie o aproveitamento de um jogador de futebol. O programa vai ler o nome do jogador e quantas partidas ele jogou. Depois vai ler a quantidade de golds feitos em cada partida. No final, tudo será guardado em um dicionário, incluindo o total de gols feitos durante o campeonato.'''
+'''93 - Crie um programa que gerencie o aproveitamento de um jogador de futebol. O programa vai ler o nome do jogador e quantas partidas ele jogou. Depois vai ler a quantidade de golds feitos em cada partida. No final, tudo será guardado em um dicionário, incluindo o total de gols feitos durante o campeona
+95 - Aprimore o desafio 93 para que ele funcione com vários jogadores, incluindo um sistema de visualização de detalhes do aproveitamento de cada jogador.'''
+
 from time import sleep
+jogadores = []
 jogador = {}
 gols = []
-jogador['nome'] = input('Nome: ')
-jogador['partidas'] = int(input('Partidas: '))
-for c in range(0, jogador['partidas']):
-    gols.append(int(input(f'Quantos gols na partida {c}: ')))
-jogador['gol'] = gols
-print(jogador)
-for i, v in enumerate(gols):
-    print(f'na partida {i + 1} fez {v} gols')
-    sleep(1)
+tot = 0
+while True:
+    jogador.clear()
+    gols.clear()
+    jogador['nome'] = input('Nome: ')
+    jogador['partidas'] = int(input('Partidas: '))
+    for c in range(0, jogador['partidas']):
+        gols.append(int(input(f'Quantos gols na partida {c + 1}: ')))
+    jogador['gol'] = gols.copy()
+    jogadores.append(jogador.copy())
+    tot = sum(jogador['gol'])
+    while True:
+        continuar = input('Continuar [S/N]: ' ).upper()[0]
+        if continuar in  'SN':
+            break
+        print('ERRO! O campo aceita apenas S ou N!')
+    if continuar == 'N':
+        break
+print(jogadores)
+print()
+print('-=' * 30)
+print(f'{"cod":<5}{"NOME":>5}{"PARTIDAS":>15}{"GOLS" :>15}')
+print()
+for i, v in enumerate(jogadores):
+    print(f'{i}')
+
     
 
 
