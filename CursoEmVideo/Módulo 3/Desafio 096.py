d1b79241cb04d918c716f641300efdbdeb2dca17
André Matos <andrermatos90@gmail.com>
2023-06-03 15:26:56 -0300

    Desafio 096

--- CursoEmVideo/Módulo 3/Desafio 096.py
+++ CursoEmVideo/Módulo 3/Desafio 096.py
@@ -2,31 +2,22 @@
 96 - Faça um programa que tenha uma função chamada area(), que receba as dimensões 
 de um terreno retangular (largura x comprimento) e mostre a área do terreno'''
 
-def area(l, c):
-    r = l * c
-    print(f'Área: {r:.2f}m²')
+def area(largura, coprimento):
+    a = largura * coprimento
+    print(f'Área: {a:.2f} m²')
 
+#prorgama principal
 l = float(input('Largura: '))
 c = float(input('Comprimento: '))
 area(l, c)
 
 '''
-def area(l, c):
+A função area é definida com dois parâmetros: largura e comprimento.
+Dentro da função, uma variável a é criada e recebe o valor da multiplicação de largura por comprimento, o que calcula a área do retângulo.
+O resultado é então impresso usando a função print e formatado como uma string com duas casas decimais usando formatação de string f: f'Área: {a:.2f}m²'.
+O código solicita ao usuário que insira a largura do retângulo usando a função input, e o valor é armazenado na variável l após ser convertido para um número de ponto flutuante usando float(input('Largura: ')).
+Da mesma forma, o código solicita ao usuário que insira o comprimento do retângulo, armazena o valor na variável c após convertê-lo para um número de ponto flutuante usando float(input('Comprimento: ')).
+Por fim, a função area é chamada com os argumentos l e c, e a área do retângulo é calculada e impressa.
 
-Esta linha define uma função chamada area com dois parâmetros: l (largura) e c (comprimento).
-r = l * c
-
-Nesta linha, o código calcula a área multiplicando a largura (l) pelo comprimento (c) e armazena o resultado na variável r.
-print(f'Área: {r:.2f}m²')
-
-Aqui, a função exibe a área calculada usando a função print(). A sintaxe {r:.2f} formata o valor de r como um número de ponto flutuante com duas casas decimais.
-l = float(input('Largura: '))
-
-Esta linha solicita ao usuário que digite a largura e armazena o valor na variável l. A função input() é usada para obter a entrada do usuário como uma string, e a função float() é usada para converter a string em um número de ponto flutuante.
-c = float(input('Comprimento: '))
-
-Aqui, o código solicita ao usuário que digite o comprimento e armazena o valor na variável c. Assim como na linha anterior, a função float() é usada para converter a entrada do usuário em um número de ponto flutuante.
-area(l, c)
-
-Esta linha chama a função area, passando os valores de l e c como argumentos. A função será executada, calculando a área e exibindo o resultado.'''
+'''
 
