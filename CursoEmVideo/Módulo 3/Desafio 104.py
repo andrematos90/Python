1fbe60b20730dcf423e6c23d27ece9a0748f4b8a
André Matos <andrermatos90@gmail.com>
2023-06-07 20:10:35 -0300

    Desafio 104

---
+++ CursoEmVideo/Módulo 3/Desafio 104.py
@@ -0,0 +1,6 @@
+'''104 - Crie um programa que tenha a função leiaInt(), que vai funcionar de forma semelhante á função 
+input() do python, só que fazendo a validação para aceitar apenas um valor númerico.
+EX: n = leiaInt('Digite um n')'''
+
+
+
