ad6280e64d9819175008ed55c546dd12781c9ae9
André Matos <andrermatos90@gmail.com>
2023-06-09 11:48:20 -0300

    Exercicios

--- CursoEmVideo/Exercicios.py
+++ CursoEmVideo/Exercicios.py
@@ -496,3 +496,20 @@ EX: n = leiaInt('Digite um n')'''
 - A situação (opcional)
 
 Adicione tambem docstrings.'''
+
+
+'''106 - aça um mini-sistema que utilize o interactive help do python.
+O usuário vai digitar o comando e o manual vai aparecer. Quando o usuário digitar a palavra "Fim", o programa encerrará. '''
+
+'''
+107 - Crie um módulo chamado moeda.py que tenha funções incorporadas
+aumentar(), diminuir(). dobro(), e metade().
+
+Faça também um programa que importe esse módulo e use algumas dessas funções'''
+
+'''108 - Adapte o código do exercicio 107, criando uma função adicional chamada moeda() que consiga mostrar
+os valores como um valor monetário formatado.'''
+
+'''109 - Modifique as funções que foram criadas no exercicios 107 para que elas aceitem um 
+parÂmetro a mais, informando se o valor retornado por elas  vai ou não ser formatado pela 
+função moeda(), desenvolvida no exercicio 108. '''
---
+++ CursoEmVideo/Módulo 3/Desafio 109.py
@@ -0,0 +1,3 @@
+'''109 - Modifique as funções que foram criadas no exercicios 107 para que elas aceitem um 
+parÂmetro a mais, informando se o valor retornado por elas  vai ou não ser formatado pela 
+função moeda(), desenvolvida no exercicio 108. '''
