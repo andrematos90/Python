ff04e52134e81e0e22b23b6824513a2fddcfa2e0
André Matos <andrermatos90@gmail.com>
2023-06-09 15:28:07 -0300

    Desafio  109

--- CursoEmVideo/Módulo 3/Desafio 109.py
+++ CursoEmVideo/Módulo 3/Desafio 109.py
@@ -1,3 +1,34 @@
 '''109 - Modifique as funções que foram criadas no exercicios 107 para que elas aceitem um 
 parÂmetro a mais, informando se o valor retornado por elas  vai ou não ser formatado pela 
 função moeda(), desenvolvida no exercicio 108. '''
+
+from pacotes import moeda
+
+v = float(input('Valor: '))
+print(f'A metade de R${v} é {moeda.metade(v, False)}')
+print(f'O dobro de R${v} é {moeda.dobro(v, False)}')
+print(f'Aumentando 10% de R${v}, fica: {moeda. aumentando(v, True)}')
+print(f'Diminuindo 13% de {v}, fica: {moeda.diminuindo(v, False)}')
+
+
+'''
+Importação do módulo moeda:
+
+O código começa importando o módulo moeda de um pacote externo. Isso permite o acesso às funções e recursos implementados nesse módulo.
+Solicitação de um valor numérico ao usuário:
+
+O código usa a função input() para solicitar ao usuário que insira um valor numérico. O valor inserido é lido como uma string.
+Conversão do valor para tipo float:
+
+O valor lido do usuário é convertido para o tipo float usando a função float(). Isso garante que o valor seja tratado como um número de ponto flutuante.
+Utilização das funções do módulo moeda:
+
+O código chama as funções do módulo moeda para realizar operações matemáticas e formatação de valores.
+A função moeda.metade() é chamada passando o valor lido como argumento e o segundo argumento é definido como False. O resultado é exibido usando a função print().
+A função moeda.dobro() é chamada da mesma maneira, passando o valor lido e False como argumentos.
+A função moeda.aumentando() é chamada com o valor lido e True como argumentos. O resultado é exibido usando a função print().
+A função moeda.diminuindo() é chamada com o valor lido e False como argumentos. O resultado é exibido usando a função print().
+Exibição dos resultados formatados:
+
+Os resultados das operações são exibidos usando a função print().
+As strings formatadas usam f-strings para incluir os valores originais e os resultados das operações.'''
--- CursoEmVideo/Módulo 3/pacotes/moeda.py
+++ CursoEmVideo/Módulo 3/pacotes/moeda.py
@@ -1,16 +1,58 @@
-def metade(numeroquequeroametade):
-    return numeroquequeroametade / 2
+def metade(numeroquequeroametade, formatado=False):
+    if formatado == False:
+        return numeroquequeroametade / 2
+    else:
+       return moeda(numeroquequeroametade)
 
-def dobro(numeroquequeroodoro):
-    return numeroquequeroodoro * 2
+def dobro(numeroquequeroodoro, formatado=False):
+    if formatado == False:
+        return numeroquequeroodoro * 2
+    else:
+        return moeda(numeroquequeroodoro * 2)
 
-def aumentando(numeroquequeroaumentar):
-    numeroquequeroaumentar = numeroquequeroaumentar + (numeroquequeroaumentar / 100) *10
-    return numeroquequeroaumentar
+def aumentando(numeroquequeroaumentar, formatado=False):
+    if formatado == False:
+        numeroquequeroaumentar = numeroquequeroaumentar + (numeroquequeroaumentar / 100) *10
+        return numeroquequeroaumentar
+    else:
+          numeroquequeroaumentar = numeroquequeroaumentar + (numeroquequeroaumentar / 100) *10
+          return moeda(numeroquequeroaumentar)
 
-def diminuindo(numeroquequerodiminmuir):
-    numeroquequerodiminmuir = numeroquequerodiminmuir - ((numeroquequerodiminmuir / 100) *  13)
-    return numeroquequerodiminmuir
+def diminuindo(numeroquequerodiminmuir, formatado=False):
+    if formatado == False:
+        numeroquequerodiminmuir = numeroquequerodiminmuir - ((numeroquequerodiminmuir / 100) *  13)
+        return numeroquequerodiminmuir
+    else:
+        numeroquequerodiminmuir = numeroquequerodiminmuir - ((numeroquequerodiminmuir / 100) *  13)
+        return moeda(numeroquequerodiminmuir)
 
 def moeda(valor):
    return f'R${valor:.2f}'
+
+'''
+Função metade(numeroquequeroametade, formatado=False):
+
+Esta função recebe um número como argumento (numeroquequeroametade) e um argumento opcional chamado formatado.
+Se formatado for False, a função retorna a metade do número fornecido (numeroquequeroametade / 2).
+Caso contrário, a função chama a função moeda passando o número fornecido como argumento e retorna o valor formatado pela função moeda.
+Função dobro(numeroquequeroodoro, formatado=False):
+
+Esta função recebe um número como argumento (numeroquequeroodoro) e um argumento opcional chamado formatado.
+Se formatado for False, a função retorna o dobro do número fornecido (numeroquequeroodoro * 2).
+Caso contrário, a função chama a função moeda passando o dobro do número fornecido como argumento e retorna o valor formatado pela função moeda.
+Função aumentando(numeroquequeroaumentar, formatado=False):
+
+Esta função recebe um número como argumento (numeroquequeroaumentar) e um argumento opcional chamado formatado.
+Se formatado for False, a função aumenta o número em 10% e retorna o valor resultante (numeroquequeroaumentar + (numeroquequeroaumentar / 100) * 10).
+Caso contrário, a função chama a função moeda passando o número aumentado em 10% como argumento e retorna o valor formatado pela função moeda.
+Função diminuindo(numeroquequerodiminmuir, formatado=False):
+
+Esta função recebe um número como argumento (numeroquequerodiminmuir) e um argumento opcional chamado formatado.
+Se formatado for False, a função diminui o número em 13% e retorna o valor resultante (numeroquequerodiminmuir - ((numeroquequerodiminmuir / 100) * 13)).
+Caso contrário, a função chama a função moeda passando o número diminuído em 13% como argumento e retorna o valor formatado pela função moeda.
+Função moeda(valor):
+
+Esta função recebe um valor numérico como argumento.
+A função formata o valor numérico fornecido como uma string formatada para representar um valor monetário com o símbolo de real (R$) e duas casas decimais (f'R${valor:.2f}').
+Em seguida, retorna a string formatada representando o valor monetário.
+Essas funções podem ser usadas para realizar cálculos matemáticos simples, como obter a metade, o dobro, aumentar ou diminuir um número em uma determinada porcentagem e formatar valores monetários.'''
