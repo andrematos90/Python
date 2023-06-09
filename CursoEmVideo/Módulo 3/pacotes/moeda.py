61a1d02dcfcee68f68745312f421cf07092b1183
André Matos <andrermatos90@gmail.com>
2023-06-09 16:16:31 -0300

    Desafio 110

--- CursoEmVideo/Módulo 3/pacotes/moeda.py
+++ CursoEmVideo/Módulo 3/pacotes/moeda.py
@@ -66,4 +66,38 @@ Função moeda(valor):
 Esta função recebe um valor numérico como argumento.
 A função formata o valor numérico fornecido como uma string formatada para representar um valor monetário com o símbolo de real (R$) e duas casas decimais (f'R${valor:.2f}').
 Em seguida, retorna a string formatada representando o valor monetário.
-Essas funções podem ser usadas para realizar cálculos matemáticos simples, como obter a metade, o dobro, aumentar ou diminuir um número em uma determinada porcentagem e formatar valores monetários.'''
+Essas funções podem ser usadas para realizar cálculos matemáticos simples, como obter a metade, o dobro, aumentar ou diminuir um número em uma determinada porcentagem e formatar valores monetários.
+
+
+
+
+FUNÇÃO RESUMO 
+
+Impressão de linhas de separação:
+
+A função imprime uma linha de caracteres '-' repetidos 30 vezes usando o operador de multiplicação (print('-' * 30)). Isso cria uma linha visualmente separada para o resumo.
+Impressão do título "RESUMO DO VALOR":
+
+A função imprime a string "RESUMO DO VALOR" usando a função print().
+Impressão de linhas de separação:
+
+A função imprime novamente uma linha de caracteres '-' repetidos 30 vezes para separar o título do restante do resumo.
+Impressão do valor analisado:
+
+A função imprime a string formatada f'Valor analisado: R${valor:.2f}'. Essa string inclui o valor fornecido como argumento com o símbolo de real (R$) e duas casas decimais.
+Impressão do dobro do valor:
+
+A função chama a função dobro() passando o valor fornecido como argumento e True como segundo argumento para obter o dobro do valor formatado. O resultado é impresso utilizando a função print().
+Impressão da metade do valor:
+
+A função chama a função metade() passando o valor fornecido como argumento e True como segundo argumento para obter a metade do valor formatada. O resultado é impresso utilizando a função print().
+Impressão do aumento de 10%:
+
+A função chama a função aumentando() passando o valor fornecido como argumento e True como segundo argumento para obter o valor aumentado em 10% formatado. O resultado é impresso utilizando a função print().
+Impressão da redução de 13%:
+
+A função chama a função diminuindo() passando o valor fornecido como argumento e True como segundo argumento para obter o valor reduzido em 13% formatado. O resultado é impresso utilizando a função print().
+Impressão de linhas de separação:
+
+A função imprime novamente uma linha de caracteres '-' repetidos 30 vezes para '''
+
