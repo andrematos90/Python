f0fb37ae39b083a7c002e332ac8d3efe31afda97
André Matos <andrermatos90@gmail.com>
2023-06-09 10:52:09 -0300

    Desafio 107

---
+++ CursoEmVideo/Módulo 3/Desafio107.py
@@ -0,0 +1,17 @@
+'''
+107 - Crie um módulo chamado moeda.py que tenha funções incorporadas
+aumentar(), diminuir(). dobro(), e metade().
+
+Faça também um programa que importe esse módulo e use algumas dessas funções'''
+
+from pacotes import moeda
+
+
+
+#programa principal
+
+p = float(input('Valor R$: '))
+print(f'A metade de {p} é {moeda.metade(p)}')
+print(f'O dobro de {p} é {moeda.dobro(p)}')
+print(f'Aumentando 10%, temos {moeda.aumentando(p)}')
+print(f'Diminuido 13%, temos {moeda.diminuindo(p)}')
---
+++ CursoEmVideo/Módulo 3/pacotes/moeda.py
@@ -0,0 +1,13 @@
+def metade(numeroquequeroametade):
+    return numeroquequeroametade / 2
+
+def dobro(numeroquequeroodoro):
+    return numeroquequeroodoro * 2
+
+def aumentando(numeroquequeroaumentar):
+    numeroquequeroaumentar = numeroquequeroaumentar + (numeroquequeroaumentar / 100) *10
+    return numeroquequeroaumentar
+
+def diminuindo(numeroquequerodiminmuir):
+    numeroquequerodiminmuir = numeroquequerodiminmuir - ((numeroquequerodiminmuir / 100) *  13)
+    return numeroquequerodiminmuir
