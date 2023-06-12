f10999e01e6603a94206671c6700415cea899773
André Matos <andrermatos90@gmail.com>
2023-06-12 16:10:40 -0300

    Desafio 108

---
+++ CursoEmVideo/Módulo 3/Desafio 108/teste.py
@@ -0,0 +1,10 @@
+'''108 - Adapte o código do exercicio 107, criando uma função adicional chamada moeda() que consiga mostrar
+os valores como um valor monetário formatado.'''
+
+import moeda
+
+v = float(input('Valor: '))
+print(f'A metade de {moeda.moeda(v)} é {moeda.moeda(moeda.metade(v))}')
+print(f'O dobro de {moeda.moeda(v)} é {moeda.moeda(moeda.dobro(v))}')
+print(f'Aumentando 10% temos {moeda.moeda(moeda.aumentar(v, 10))}')
+print(f'Diminuindo 13% temos {moeda.moeda(moeda.dimunuir(v, 13))}')
