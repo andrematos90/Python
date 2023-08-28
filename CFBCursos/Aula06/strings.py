61c807f07975937c90fc5838e5756590f3bafd71
André Matos <andrermatos90@gmail.com>
2023-08-28 15:16:43 -0300

    Aula 06 - strings

---
+++ CFBCursos/Aula06/strings.py
@@ -0,0 +1,8 @@
+string = " André Rosa de Matos " 
+print(string[0:2].upper()) #imprime da posição 0 até a 2 e transforma em maiusculo
+print(string.lower()) # transforma tudo em minusculo
+print(len(string)) #quantidade de caracteres
+print(string.strip()) # remove os espaços do inicio e do final
+print(string.replace("André", "Simba")) #substitui uma string por outra
+print(string.split(" ")) #divide a string e retorna um array
+
