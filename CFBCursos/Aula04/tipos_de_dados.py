bda039661f6b0cc6f7793c58f1db5f4ffd2fbb52
André Matos <andrermatos90@gmail.com>
2023-08-28 12:02:47 -0300

    Aula 04 - tipos de dados

---
+++ CFBCursos/Aula04/tipos_de_dados.py
@@ -0,0 +1,14 @@
+inteiro = 1 #inteiro
+string = 'André' # string
+numero_real = 15.5 # float
+booleano = True # bool
+
+lista_ou_array = ["carro", "aviao", "bicicleta", 1.45, False] #lista
+tupla = ("carro", "aviao", "bicicleta", 1.45, False) #tupla é uma lista não mutável
+
+dicionario = { "nome" : "andré",
+               "sobrenome" : "matos"
+}
+
+tipo_set = {5, 7, 8, 7, 4, 5} #tipo set
+
