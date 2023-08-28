7a729ae404e75f09981279a38c835d66d4e4e1ea
André Matos <andrermatos90@gmail.com>
2023-08-28 14:24:17 -0300

    Aula 05 - tipos numéricos

---
+++ CFBCursos/Aula05/tipos_de_numericos.py
@@ -0,0 +1,13 @@
+import random
+
+num_i = 10 #tipo inteiro
+num_f = 5.2 # tipo float
+num_c = 1j #tipo complexo
+
+x = int(num_f)
+
+print("valor: " + str(x) + " - Tipo: " + str(type(x)))
+
+
+aleatorio = [random.randrange(0, 100), random.randrange(0, 100), random.randrange(0, 100), ]
+print(aleatorio)
